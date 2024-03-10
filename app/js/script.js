b4184d867aa46582b0dfaef020afae208390d752
sherryliang <sherry01214@gmail.com>
2024-03-10 22:28:11 +0800

    add btn menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log("hello worlllllld");
+const btnHamburger = document.querySelector('#btnHamburger');
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }
+    else{
+        btnHamburger.classList.add('open');
+    }
+});
